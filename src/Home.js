647cb4654f741845b708582b819924f9a43cf575
morgannegagne <morgannegagne@gmail.com>
2018-03-22 17:46:09 -0400

    added fetch requests for doctors and google maps

--- src/Home.js
+++ src/Home.js
@@ -4,28 +4,62 @@ export default class Home extends React.Component {
 
   state = {
     doctors: [],
-    latitude: null,
-    longitude: null
+    latitude: 40.7047751,
+    longitude: -74.013277,
+    input: ''
   }
 
   setPosition = (position) => {
-      console.log(position)
-      this.setState({latitude: position.coords.latitude, longitude: position.coords.longitude})
+    console.log(position);
+      this.setState({latitude: position.coords.longitude, longitude: position.coords.latitude}, this.fetchDoctors)
   }
 
-  componentDidMount(){
+  getLocation() {
     if (navigator.geolocation) {
+      console.log('getting position')
       navigator.geolocation.getCurrentPosition(this.setPosition);
     } else {
       console.log("Geolocation is not supported by this browser.");
     }
   }
 
+  fetchDoctors = () => {
+    fetch(`http://Localhost:3000/api/v1/doctor_database?longitude=${this.state.longitude}&latitude=${this.state.latitude}`)
+      .then(res => res.json())
+      .then(doctors => this.setState({doctors}))
+  }
+
+  clicked = (e) => {
+    e.preventDefault();
+    let options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        accept: "application/json"
+      },
+      body: JSON.stringify({
+        "location": this.state.input
+      })
+    }
+    console.log(options)
+    fetch('http://localhost:3000/api/v1/google', options)
+      .then(res => res.json())
+      .then(json => console.log(json))
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      input: e.target.value
+    })
+  }
+
   render(){
-    console.log(this.state)
     return(
       <div>
-        Home
+        <form>
+          <input type="text" onChange={this.handleChange}/>
+          <button onClick={this.clicked}>Submit</button>
+        </form>
       </div>
     )
   }
