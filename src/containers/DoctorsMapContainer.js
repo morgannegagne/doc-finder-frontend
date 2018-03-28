655dcfeb8c719b8d37f1ca379ad05bb0644678e5
morgannegagne <morgannegagne@gmail.com>
2018-03-28 14:34:42 -0400

    updated api key

--- src/containers/DoctorsMapContainer.js
+++ src/containers/DoctorsMapContainer.js
@@ -36,11 +36,12 @@ export default class DoctorsMapContainer extends React.Component {
 
 	render() {
 		const doctors = this.findClosestPractice()
+		console.log(process.env)
 		return (
 				<DoctorsMap
 					doctors={doctors}
 					location={this.state.location}
-					googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_apiKey}&v=3.exp&libraries=geometry,drawing,places`}
+					googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.apiKey}&v=3.exp&libraries=geometry,drawing,places`}
 					loadingElement={<div style={{ height: `100%` }} />}
 					containerElement={<div style={{ height: `600px`, width: `600px` }} />}
 					mapElement={<div style={{ height: `100%` }} />}
